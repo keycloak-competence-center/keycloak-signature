e63d44b13fe345c8b0f6f6040fd3f42105cabe0b
Peter Siska <63866+peschee@users.noreply.github.com>
2023-09-20 09:25:43 +0200

    Add CEM config

---
+++ extensions/extension-signature/src/main/web/custom-elements-manifest.config.js
@@ -0,0 +1,7 @@
+// @see https://custom-elements-manifest.open-wc.org/analyzer/config/
+export default {
+  globs: ['src/**/*.ts'],
+  exclude: ['src/**/*.spec.ts'],
+  litelement: true,
+  packagejson: true,
+};
