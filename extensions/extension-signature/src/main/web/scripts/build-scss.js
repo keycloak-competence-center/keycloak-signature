bf51565ec7abb4351fb8c6ee8e0dcc2370f83a0c
Peter Siska <63866+peschee@users.noreply.github.com>
2023-09-22 10:45:28 +0200

    Run prettier when building scss

--- extensions/extension-signature/src/main/web/scripts/build-scss.js
+++ extensions/extension-signature/src/main/web/scripts/build-scss.js
@@ -3,12 +3,16 @@ import { basename, dirname } from 'node:path';
 import fg from 'fast-glob';
 import { compileString } from 'sass';
 import stylelint from 'stylelint';
+import * as prettier from 'prettier';
 
 const files = await fg('./src/**/*.scss');
 
 const shared = process.argv.at(3) || '',
   sharedFile = basename(shared),
   sharedDir = dirname(shared);
+
+// Resolve prettier config
+const prettierOptions = await prettier.resolveConfig(process.cwd());
 
 await Promise.allSettled(
   files.map(async (file) => {
@@ -32,11 +36,15 @@ await Promise.allSettled(
         .map((string_) => `  ${string_}`.trimEnd())
         .join('\n');
 
-      // Step 3: write CSS to TS template
-      await fs.writeFile(
-        `${file}.ts`,
-        `import { css } from 'lit';\nexport default css\`\n${output}\`;\n`
-      );
+      // Step 3: Format with Prettier
+      output = `import { css } from 'lit';\nexport default css\`\n${output}\`;\n`;
+      output = await prettier.format(output, {
+        ...prettierOptions,
+        parser: 'typescript',
+      });
+
+      // Step 4: write CSS to TS template
+      await fs.writeFile(`${file}.ts`, output);
     } catch (error) {
       console.log(error);
     }
