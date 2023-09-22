a3bf079409a70566709605f3951c26f2fbe7133c
David Bidjan <david.bidjan@inventage.com>
2023-09-22 17:35:06 +0200

    wip

--- extensions/extension-signature/src/main/java/com/inventage/keycloak/signature/infrastructure/token/PayloadToken.java
+++ extensions/extension-signature/src/main/java/com/inventage/keycloak/signature/infrastructure/token/PayloadToken.java
@@ -9,6 +9,9 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.services.Urls;
 
+/**
+ * Custom Token
+ */
 public class PayloadToken extends DefaultActionToken {
 
     public static final String TOKEN_TYPE = "signed-payload-token";
